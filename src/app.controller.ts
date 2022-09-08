104c7b059aaa0440d7eb58f3cb50a9a21f008d99
RicholeYu <richole.yu@qq.com>
2022-09-08 10:07:58 +0800

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,26 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import * as crypto from 'crypto';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as wechat from 'wechat-jssdk';
-import { Request } from 'express';
 import config from './wxConfig.json';
-
-const wx = new wechat.Wechat(config);
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/')
-  async getSignature(@Req() request: Request) {
-    console.log(request.url);
-    const signatureData: any = await wx.jssdk.getSignature(request.url);
-    console.log(signatureData);
+  @Get('/wechat/get-signature')
+  async getSignature(@Query() query) {
+    const arr = [config.wechatToken, query.timestamp, query.nonce].sort();
+    const sha1Hash = crypto.createHash('sha1');
+    sha1Hash.update(arr.join(''));
 
-    return signatureData;
+    const result = sha1Hash.digest('hex');
+
+    if (result === query.signature) {
+      Logger.log('微信公众号验证成功');
+      return query.echostr;
+    }
+
+    Logger.error('微信公众号验证失败');
+    return 'failed';
   }
 }
