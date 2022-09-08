c37d0e11f0110704eedd65dd559b0cb9d139272c
RicholeYu <richole.yu@qq.com>
2022-09-08 11:11:19 +0800

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import Config from './wxConfig.json';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  accessToken: string;
+
+  constructor(private readonly httpService: HttpService) {
+    this.getAccessToken();
+  }
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async getAccessToken() {
+    const response = await firstValueFrom(
+      this.httpService.get(
+        `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${Config.appId}&secret=${Config.appSecret}`,
+      ),
+    );
+
+    console.log(response.data);
+    this.accessToken = response.data.accessToken;
   }
 }
